e7b2c774a4bc5c9232a4e7c6bc1afd30cce48c3b
luana monteiro <monteiroluana194548@gmail.com>
2025-09-11 09:59:54 -0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,16 +1,20 @@
-print("-------------NOTA FISCAL COMPRA-----------")
+print("="*50)
+print("NOTA FISCAL COMPRA". center(50))
+print("="*50)
 
 total_geral = 0
 subtotal_geral = 0
 desconto_geral = 0
 
-
 for i in range (1,6):
-    print(f'-----------------Produto {i}----------------')
+    print("\n"+"-"*50)
+    print(f"PRODUTO {i}".center(50))
+    print("-"*50)
+    
     # declarando varáveis e entradas
-    desc_prod = input(f'Digite a descrição do produto: ')
-    qtd_adq = int(input(f'Digite a quantidade adquirida: '))
-    preco_prod = float(input(f'Digite o preço: R$ '))
+    desc_prod = input(f'Descrição do produto: ')
+    qtd_adq = int(input(f'Quantidade adquirida: '))
+    preco_prod = float(input(f'Preço Unitário: R$ '))
 
     # calculando subtotal, descontos e total
 
@@ -18,34 +22,34 @@ for i in range (1,6):
 
     if qtd_adq <= 5:
         desconto = 0.02
-        desconto_msg = "2%"
     elif qtd_adq > 5 and qtd_adq <= 10:
         desconto = 0.03
-        desconto_msg = "3%"
     else:
         desconto = 0.05
-        desconto_msg = "5%"
 
     desconto_valor = (subtotal*desconto)
     total = subtotal-(subtotal*desconto)
-
+    desconto_msg = f"{int(desconto*100)}%"
     # printando resultados 
 
-    print("------------------------------------------")
-    print(f'Subtotal Produto: R${subtotal:.2f}')
-    print(f'Aplicando {desconto_msg} de desconto: R$-{desconto_valor:.2f}')
-    print(f'Total Produto: R${total:.2f}')
-    print("------------------------------------------")
+    print("\n"+"-"*50)
+    print(f'Subtotal: R${subtotal:,.2f}')
+    print(f'Desconto Aplicado ({desconto_msg}): R$-{desconto_valor:,.2f}')
+    print(f'Total com Desconto: R${total:,.2f}')
+    print("-"*50)
 
     # somando valores de 5 produtos e printando totais
 
     subtotal_geral += subtotal
     desconto_geral += desconto_valor
     total_geral += total
+    
+#resumo final da compra
 
-print("---------------------------------------")
-print("-------------RESUMO DA COMPRA-----------")
-print(f'Subtotal Geral: R${subtotal_geral:.2f}')
-print(f'Desconto Geral: R$-{desconto_geral:.2f}')
-print(f'Total Geral: R${total_geral:.2f}')
-print("---------------------------------------")
+print("\n"+"="*50)
+print("RESUMO DA COMPRA".center(50))
+print("="*50)
+print(f'Subtotal Geral: R${subtotal_geral:,.2f}')
+print(f'Desconto Total: R$-{desconto_geral:,.2f}')
+print(f'Total A Pagar: R${total_geral:,.2f}')
+print("\n"+"="*50)
